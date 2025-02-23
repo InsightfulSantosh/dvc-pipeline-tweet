cab578f71af432e2dc0ffa8469f603ffa384fd53
Santosh Kumar <studenteearya@gmail.com>
2025-02-23 10:18:20 +0530

    model building initiated

--- src/model/model_building.py
+++ src/model/model_building.py
@@ -1,13 +1,6 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
 import pickle
 import os
-from sklearn.metrics import (
-    accuracy_score, classification_report, confusion_matrix, 
-    f1_score, precision_score, recall_score, roc_curve, roc_auc_score
-)
 from sklearn.tree import DecisionTreeClassifier
 
 def load_data(df):
@@ -32,16 +25,11 @@ def save_model(model, model_dir):
 def main():
     # Load data
     train_path = "/Users/santoshkumar/Data_science/dvc-pipeline-tweet/data/processed/train_bow.csv"
-    test_path = "/Users/santoshkumar/Data_science/dvc-pipeline-tweet/data/processed/test_bow.csv"
-
     train_df = load_data(train_path)
-    test_df = load_data(test_path)
 
     # Split data
     X_train = train_df.drop('label', axis=1)
     y_train = train_df['label']
-    X_test = test_df.drop('label', axis=1)
-    y_test = test_df['label']
     print('Data loaded successfully')
     print("X_train shape: ", X_train.shape)
 
